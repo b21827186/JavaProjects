dae4207dcfeb079fdf1375bb55e4b9b8b7e274d2
Misbah Bilgili <56787837+b21827186@users.noreply.github.com>
2022-02-04 15:16:24 +0300

    Add files via upload

--- Payroll Monitoring University/Chief.java
+++ Payroll Monitoring University/Chief.java
@@ -1,1 +1,18 @@
 
+public class Chief extends Fulltime_Employee{
+
+	public Chief() {
+		super("C");
+		
+	}
+	public Double total(int work_for_week1,int work_for_week2,int work_for_week3,int work_for_week4,int year_of_start){
+		Double week1 = overWorkSalary(work_for_week1);
+		Double week2 = overWorkSalary(work_for_week2);
+		Double week3 = overWorkSalary(work_for_week3);
+		Double week4 = overWorkSalary(work_for_week4);
+		Double severance = severancePay(year_of_start);
+		Double dayofwork = dayOfWork();
+		return dayofwork + week1 + week2 + week3 + week4 + severance ;
+	}
+
+}
